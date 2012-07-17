cf8a8e602644e0a094bfec39a2f38914f2599e8c
Sean Yu <seansu4you87@gmail.com>
2012-07-16 17:24:24 -0700

    authorize before filters added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  #before_filter :authorize
+  before_filter :authorize_user, :only =>[]
+  #before_filter :authorize_admin, :only =>[ :edit, :update, :destroy, :create ]
   
   protected
   
@@ -12,9 +13,20 @@ class ApplicationController < ActionController::Base
     User.find(session[:user_id])
   end
   
-  def authorize
+  def authorize_user
     unless self.current_user
-      redirect_to login_url, notice: 'Please log in first'
+      redirect_to login_url, alert: 'Please log in first'
+    end
+  end
+  
+  def authorize_admin
+    if self.current_user == nil
+      self.authorize_user
+      return
+    end
+    
+    unless self.current_user.admin
+      redirect_to login_url, alert: 'Please log in as an admin'
     end
   end
   
